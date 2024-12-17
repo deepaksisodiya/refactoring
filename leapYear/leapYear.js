ad0d0817aa30bc9131e378e63b2f6e88cbd45109
yogendra jaiswal <yogendra@incubyte.co>
2024-12-17 10:53:38 +0530

    refactor leap year logic and enhance unit tests

--- leapYear/leapYear.js
+++ leapYear/leapYear.js
@@ -1,12 +1,20 @@
-
-
 function isLeapYear(year) {
-    if (year % 4 === 0) {
-        return true;
-    }
+  if (isNumDivisibleBy(year, 400)) {
+    return true;
+  }
+  if (isNumDivisibleBy(year, 100)) {
     return false;
+  }
+  if (isNumDivisibleBy(year, 4)) {
+    return true;
+  }
+  return false;
+}
+
+function isNumDivisibleBy(num, divider) {
+  return num % divider === 0;
 }
 
 module.exports = {
-    isLeapYear
-}
+  isLeapYear,
+};
--- leapYear/leapYear.test.js
+++ leapYear/leapYear.test.js
@@ -1,13 +1,19 @@
-import { isLeapYear } from './leapYear';
+import { isLeapYear } from "./leapYear";
 
-describe('isLeapYear', () => {
-    it('1997 should not be a leap year', () => {
-        expect(isLeapYear(1997)).toBe(false);
-    });
+describe("isLeapYear", () => {
+  it("should return false for 1997", () => {
+    expect(isLeapYear(1997)).toBe(false);
+  });
 
-    it('1996 should be a leap year', () => {
-        expect(isLeapYear(1996)).toBe(true);
-    });
+  it("should return true for 1996", () => {
+    expect(isLeapYear(1996)).toBe(true);
+  });
+
+  it("should return true for 1600", () => {
+    expect(isLeapYear(1600)).toBe(true);
+  });
+
+  it("should return false for 1800", () => {
+    expect(isLeapYear(1800)).toBe(false);
+  });
 });
-
-  
